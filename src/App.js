49dcdac9ef8aab21ad37b483de7f39b090eec1ce
LuisMR001 <70413606+LuisMR001@users.noreply.github.com>
2021-04-01 07:25:13 -0500

    Update App.js
    try sign in again

--- src/App.js
+++ src/App.js
@@ -9,6 +9,8 @@ function App() {
         <img src={bed} className="App-logo" alt="logo" />
         <h1>Rise and shine</h1>  
         <h1>Time to start those assignments</h1>
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
       </header>
     </div>
   );
