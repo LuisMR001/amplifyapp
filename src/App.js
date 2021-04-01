f85667f2819f964d486deac418c6ff01930a7a1b
LuisMR001 <70413606+LuisMR001@users.noreply.github.com>
2021-04-01 06:19:41 -0500

    Update App.js
    adding homer

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <img src={bed} className="App-logo" alt="logo" />
         <h1>Hello from V2</h1>  
         <h1>Gona Start changes</h1> 
       </header>
