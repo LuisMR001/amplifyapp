260817eed03a871a6342584d58b257f86ad350cc
LuisMR001 <70413606+LuisMR001@users.noreply.github.com>
2021-04-01 07:21:45 -0500

    Update App.js
    undo changes

--- src/App.js
+++ src/App.js
@@ -9,11 +9,6 @@ function App() {
         <img src={bed} className="App-logo" alt="logo" />
         <h1>Rise and shine</h1>  
         <h1>Time to start those assignments</h1>
-        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
-        <div class="form-floating">
-        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
-        <label for="floatingInput">Email address</label>
-        </div>
       </header>
     </div>
   );
