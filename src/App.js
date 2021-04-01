f414e1f873ac3b3faf88cc7669207774b0f9f23e
LuisMR001 <70413606+LuisMR001@users.noreply.github.com>
2021-04-01 05:41:00 -0500

    add some text

--- src/App.js
+++ src/App.js
@@ -6,7 +6,8 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <h1>Hello from V2</h1>        
+        <h1>Hello from V2</h1>  
+        <h1>Gona Start changes</h1> 
       </header>
     </div>
   );
