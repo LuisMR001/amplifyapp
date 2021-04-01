5cf403a18fcef5b0f6ae05303f56a56861942e8e
LuisMR001 <70413606+LuisMR001@users.noreply.github.com>
2021-04-01 07:19:38 -0500

    Update App.js
    trying to add sign in

--- src/App.js
+++ src/App.js
@@ -8,7 +8,12 @@ function App() {
       <header className="App-header">
         <img src={bed} className="App-logo" alt="logo" />
         <h1>Rise and shine</h1>  
-        <h1>Time to start those assignments</h1> 
+        <h1>Time to start those assignments</h1>
+        <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+        <div class="form-floating">
+        <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com">
+        <label for="floatingInput">Email address</label>
+        </div>
       </header>
     </div>
   );
